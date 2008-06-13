86c9397a18729e9d8f23ea97860281f23dee80c9
Lars Yencken <lljy@csse.unimelb.edu.au>
2008-06-13 16:43:03 +1000

    Added the basic django project structure.

---
+++ urls.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# 
+#  urls.py
+#  kanji_test
+#  
+#  Created by Lars Yencken on 2008-06-13.
+#  Copyright 2008-06-13 Lars Yencken. All rights reserved.
+# 
+
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^kanji_test/', include('kanji_test.foo.urls')),
+
+    # Uncomment this for admin:
+#     (r'^admin/', include('django.contrib.admin.urls')),
+)
