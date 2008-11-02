c8bc3f8309585f952f1b429a8de2c3b7fa609fad
Lars Yencken <lljy@csse.unimelb.edu.au>
2008-11-03 09:53:18 +1100

    Provided a screen title for management commands.

--- kanji_test/manage.py
+++ kanji_test/manage.py
@@ -8,13 +8,17 @@
 #  Copyright 2008-06-13 Lars Yencken. All rights reserved.
 # 
 
+import sys
+from cjktools.shell import setScreenTitle
+
 from django.core.management import execute_manager
 try:
     import settings # Assumed to be in the same directory.
 except ImportError:
-    import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
 
 if __name__ == "__main__":
+    if len(sys.argv) > 1 and not sys.argv[1].startswith('-'):
+        setScreenTitle('%s (manage.py)' % sys.argv[1])
     execute_manager(settings)
