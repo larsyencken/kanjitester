764c5108107417252aa655ec8f8d10e0b8ab6597
Lars Yencken <lljy@csse.unimelb.edu.au>
2008-11-10 17:12:40 +1100

    Changed the "build" management command to build any app with a build() method.

--- kanji_test/drill_tutor/management/commands/build.py
+++ kanji_test/drill_tutor/management/commands/build.py
@@ -11,21 +11,33 @@
 A command to run the build method of all apps.
 """
 
+from os import path 
+
 from django.core.management.base import NoArgsCommand
+from django.conf import settings 
 import consoleLog
 
 from kanji_test.lexicon import load_lexicon
 from kanji_test.user_model import plugin_api
 from kanji_test.user_model import add_syllabus
 
+_log = consoleLog.default
+
 class Command(NoArgsCommand):
     help = "Builds all required static database tables."
     requires_model_validation = True
 
     def handle_noargs(self, **options):
-        consoleLog.default.start('Building kanji_test', nSteps=2)
-        load_lexicon.load_lexicon()
-        add_syllabus.add_all_syllabi()
-        consoleLog.default.finish()
+        apps_with_build = []
+        for app_path in settings.INSTALLED_APPS:
+            base_module = __import__(app_path)
+            app_module = reduce(getattr, app_path.split('.')[1:], base_module)
+            if hasattr(app_module, 'build'):
+                apps_with_build.append((app_path, app_module))
+
+        _log.start('Building kanji_test', nSteps=len(apps_with_build))
+        for app_path, app_module in apps_with_build:
+            app_module.build()
+        _log.finish()
 
 # vim: ts=4 sw=4 sts=4 et tw=78:
