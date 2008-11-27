c5d936bcb0b3bc98fd6be77e70cda87cbbb9653e
Lars Yencken <lljy@csse.unimelb.edu.au>
2008-11-28 10:35:28 +1100

    [329] Adds a test case which catches this segment query bug.

--- kanji_test/plugins/test_reading_alt.py
+++ kanji_test/plugins/test_reading_alt.py
@@ -12,7 +12,7 @@ import unittest
 
 from django.contrib.auth.models import User
 
-from kanji_test.user_model.models import PartialKanji, Syllabus, ErrorDist
+from kanji_test.user_model import models
 from kanji_test.plugins import reading_alt
 
 def suite():
@@ -26,15 +26,36 @@ class ReadingAltQuestionTest(unittest.TestCase):
         User.objects.filter(username='test_user').delete()
         test_user = User(username='test_user')
         test_user.save()
-        self.syllabus = Syllabus.objects.get(tag='jlpt 4')
-        test_user.userprofile_set.create(syllabus=self.syllabus)
-        ErrorDist.init_from_priors(test_user)
         self.user = test_user
 
         self.factory = reading_alt.ReadingAlternationQuestions()
 
+    def _init_syllabus(self, tag):
+        self.syllabus = models.Syllabus.objects.get(tag=tag)
+        self.user.userprofile_set.all().delete()
+        self.user.errordist_set.all().delete()
+
+        self.user.userprofile_set.create(syllabus=self.syllabus)
+        models.ErrorDist.init_from_priors(self.user)
+
+    def test_bug_329(self):
+        self._init_syllabus('jlpt 3')
+        partial_lexeme = models.PartialLexeme.objects.get(
+                syllabus=self.syllabus,
+                lexeme__surface_set__surface=u'別に',
+                lexeme__reading_set__reading=u'べつに',
+            )
+        
+        # [329] failure in segment query here
+        question = self.factory.get_question(partial_lexeme, self.user)
+
+        # If we get here, it worked.
+        question.options.all().delete()
+        question.delete()
+
     def test_bug_325(self):
-        partial_kanji = PartialKanji.objects.get(kanji__kanji=u'後',
+        self._init_syllabus('jlpt 4')
+        partial_kanji = models.PartialKanji.objects.get(kanji__kanji=u'後',
                 syllabus=self.syllabus)
         
         # [325] an infinite loop here
