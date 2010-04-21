84b92050fa67909cd1c0167b495c34708d0ce37a
Lars Yencken <lljy@csse.unimelb.edu.au>
2010-04-22 00:16:06 +1000

    Updates the log export script to export in JSON format.

--- kanji_test/scripts/export_logs.py
+++ kanji_test/scripts/export_logs.py
@@ -13,85 +13,74 @@ from __future__ import with_statement
 import os, sys, optparse
 import datetime
 import itertools
-from xml.etree.ElementTree import Element, SubElement, tostring 
 from consoleLog import default as _log
 from consoleLog import withProgress
+import simplejson
 
 from kanji_test.user_profile import models as userprofile_models
 from kanji_test.drill import models as drill_models
+from django.contrib.auth.models import User 
 
 EXCLUDE_EMAILS = (
         'lljy@csse.unimelb.edu.au',
+        'tim@csse.unimelb.edu.au',
     )
 
 def export_logs():
     _log.start('Dumping logs', nSteps=2)
 
-    _log.start('Sanitizing logs', nSteps=2)
-    root = Element('kanji-test-log')
-    user_ids = add_user_info(root)
-    earliest, latest = add_questions_and_responses(root, user_ids)
+    latest = drill_models.Response.objects.all().order_by('-timestamp'
+            )[0].timestamp
+    datestamp = '%s_%.02d_%.02d' % (latest.year, latest.month, latest.day)
+    _dump_users('user_metadata_%s.json' % datestamp)
+    _dump_responses('user_responses_%s.json' % datestamp)
+
     _log.finish()
 
-    filename = 'kanji_test_logs_%s_%s_%s.xml' % (latest.year, latest.month,
-            latest.day)
-    _log.log('Writing to %s' % filename)
+def _dump_users(filename):
+    _log.log(filename)
     with open(filename, 'w') as ostream:
-        ostream.write(tostring(root, 'UTF-8'))
-    _log.finish()
+        profiles = userprofile_models.UserProfile.objects.exclude(
+                user__email__in=EXCLUDE_EMAILS).order_by('user__id')
 
-def add_user_info(root):
-    user_ids = []
-    user_info = SubElement(root, 'user-info')
-    profiles = list(userprofile_models.UserProfile.objects.exclude(
-            user__email__in=EXCLUDE_EMAILS))
-    profiles.sort(key=lambda u: u.user.email)
-
-    profiles = [(u, list(p)) for (u, p) in itertools.groupby(profiles,
-            lambda x: x.user.id)]
-    _log.log('Listing users ', newLine=False)
-    for user_id, user_profiles in withProgress(profiles):
-        profile = max(user_profiles, key=lambda x: x.id)
-        syllabi = ','.join(p.syllabus.tag for p in user_profiles)
-        user = SubElement(user_info, 'user', {'id': str(int(user_id))})
-        user.attrib['first-language'] = profile.first_language
-        if profile.second_languages:
-            second_languages = profile.second_languages.split(',')
-            second_languages = [l.strip() for l in second_languages]
-            user.attrib['second-languages'] = ','.join(second_languages)
-        user.attrib['syllabus'] = profile.syllabus.tag
-
-        user_ids.append(profile.user_id)
-
-    return user_ids
-
-def add_questions_and_responses(root, user_ids):
-    responses = SubElement(root, 'responses')
-    _log.log('Analysing responses ', newLine=False)
-    earliest = datetime.datetime.now()
-    latest = datetime.datetime.now() - datetime.timedelta(365 * 20)
-    for user_id in withProgress(user_ids):
-        for response in drill_models.MultipleChoiceResponse.objects.filter(
-                user__id=user_id):
-            r = SubElement(root, 'response')
-            r.attrib['user-id'] = str(int(user_id))
-            r.attrib['timestamp'] = response.timestamp.ctime()
-            earliest = min(response.timestamp, earliest)
-            latest = max(response.timestamp, latest)
-            question = response.question
-            q = SubElement(r, 'question')
-            q.attrib['type'] = question.question_type
-            q.attrib['plugin'] = question.question_plugin.name
-            q.attrib['adaptive'] = str(question.question_plugin.is_adaptive)
-
-            for option in question.multiplechoicequestion.options.all():
-                o = SubElement(q, 'option', {'value': option.value})
-                if option.is_correct:
-                    o.attrib['is-correct'] = '1'
-                if option == response.option:
-                    o.attrib['chosen'] = '1'
-
-    return (earliest, latest)
+        for user_profile in profiles:
+            record = {
+                        'user_id': user_profile.user.id,
+                        'first_language': user_profile.first_language,
+                        'second_languages':[
+                            l.strip().title() for l in \
+                            user_profile.second_languages.split(',')
+                        ],
+                        'syllabus': user_profile.syllabus.tag,
+                    }
+            print >> ostream, simplejson.dumps(record)
+        
+def _dump_responses(filename):
+    _log.log(filename, newLine=False)
+    with open(filename, 'w') as ostream:
+        earliest = datetime.datetime.now()
+        latest = datetime.datetime.now() - datetime.timedelta(365 * 20)
+        for user in withProgress(User.objects.all()):
+            for response in drill_models.MultipleChoiceResponse.objects.filter(
+                    user=user):
+                question = response.question
+                test_set = drill_models.TestSet.objects.get(questions=question)
+                record = {
+                        'user_id': user.id,
+                        'test_id': test_set.id,
+                        'timestamp': response.timestamp.ctime(),
+                        'pivot': question.pivot,
+                        'pivot_type': question.pivot_type,
+                        'question_type': question.question_type,
+                        'is_adaptive': question.question_plugin.is_adaptive,
+                        'distractors': [
+                                o.value for o in \
+                                question.multiplechoicequestion.options.all()
+                            ],
+                        'correct_response': question.multiplechoicequestion.options.get(is_correct=True).value,
+                        'user_response': response.option.value,
+                    }
+                print >> ostream, simplejson.dumps(record)
 
 #----------------------------------------------------------------------------#
 
